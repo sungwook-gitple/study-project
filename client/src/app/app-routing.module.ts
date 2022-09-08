af2fb8864b608e14ae0d059da2a2f56a41bc84cc
sungwook-gitple <swkim@gitplecorp.com>
2022-09-08 18:39:42 +0900

    🚧 working router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticatedComponent } from './authenticated/authenticated.component';
+import { ChatComponent } from './chat/chat.component';
+import { RoomListComponent } from './room-list/room-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthenticatedComponent },
+  {
+    path: 'rooms',
+    component: RoomListComponent,
+  },
+  { path: 'chat', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
